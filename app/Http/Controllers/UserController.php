f7826d01cc0d657e64a117e45ec29cc12c40accc
TatuMonNote <ezurkanmn@gmail.com>
2021-09-15 23:26:51 -0300

    Register, login and logout functionalities

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
+
+class UserController extends Controller
+{
+    public function register(){
+        $data = request()->validate([
+            'email' => 'email|unique:users,email|email:rfc,dns|required',
+            'password' => 'min:7|required|confirmed',
+            'password_confirmation' => 'required'
+        ]);
+        $data['password'] = bcrypt($data['password']);
+
+        $user = User::create($data);
+
+        Auth::login($user);
+
+        return back();
+    }
+
+    public function login(){
+        $data = request()->validate([
+            'email' => 'email|required',
+            'password' => 'required'
+        ]);
+
+        if(Auth::attempt($data)){
+            session()->regenerate();
+
+            return back();
+        }
+
+        throw ValidationException::withMessages(['Wrong email or password']);
+    }
+
+    public function destroy(){
+        Auth::logout();
+
+        return back();
+    }
+}
--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -1,26 +1,56 @@
 <header>
-    <div id="register" x-data="{show : false}" @click.away="show = false">
-        <button @click="show = !show">Register</button>
-        <form action="/register" method="POST" id="register-form" class="header-form" x-show="show">
-            <label for="email">e-mail</label>
-            <input type="email" name="email" autocomplete="email" value="{{ old('email') }}">
+    @auth
+        <a href="/logout">Log Out</a>
+    @else
+        <div id="register" x-data="{show : false}" @click.away="show = false">
+            <button @click="show = !show">Register</button>
+            <form action="/register" method="POST" id="register-form" class="header-form" x-show="show">
+                @csrf
+                <label for="email">e-mail</label>
+                <input type="email" name="email" autocomplete="email" value="{{ old('email') }}" required>
 
-            <label for="password">password</label>
-            <input type="password" name="password">
+                <label for="password">password</label>
+                <input type="password" name="password" required>
 
-            <label for="confirm-psw">confirm password</label>
-            <input type="password" name="confirm-psw">
-        </form>
-    </div>
-    
-    <div id="login" x-data="{show : false}" @click.away="show = false">
-        <button @click="show = !show">Log In</button>
-        <form action="/login" method="POST" id="login-form" class="header-form" x-show="show">
-            <label for="email">email</label>
-            <input type="email" name="email" autocomplete="email" value="{{ old('email') }}">
+                <label for="confirm-psw">confirm password</label>
+                <input type="password" name="password_confirmation" required>
 
-            <label for="password">password</label>
-            <input type="password" name="password">
-        </form>
-    </div>
+                <input type="submit" value="register">
+
+                @error('email')
+                    <p class="error-msg">{{ $message }}</p>
+                @enderror
+
+                @error('password')
+                    <p class="error-msg">{{ $message }}</p>
+                @enderror
+
+                @error('password_confirmation')
+                    <p class="error-msg">{{ $message }}</p>
+                @enderror
+            </form>
+        </div>
+        
+        <div id="login" x-data="{show : false}" @click.away="show = false">
+            <button @click="show = !show">Log In</button>
+            <form action="/login" method="POST" id="login-form" class="header-form" x-show="show">
+                @csrf
+                <label for="email">email</label>
+                <input type="email" name="email" autocomplete="email" value="{{ old('email') }}" required>
+
+                <label for="password">password</label>
+                <input type="password" name="password" required>
+
+                <input type="submit" value="log in">
+
+                @error('email')
+                    <p class="error-msg">{{ $message }}</p>
+                @enderror
+
+                @error('password')
+                    <p class="error-msg">{{ $message }}</p>
+                @enderror
+            </form>
+        </div>
+    @endauth
 </header>
